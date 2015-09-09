d071a6e214f0996f18876e6e329afbd91a886b22
Zan Loy <zan.loy@gmail.com>
2015-09-09 09:16:02 -0400

    fixed issue with uploads

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -6,7 +6,6 @@ class Attachment < ActiveRecord::Base
   belongs_to :ticket, counter_cache: true
 
   # Validations
-  validates :ticket, presence: true
   validates_attachment :file, content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/gif', 'image/png', 'application/pdf'] }
 
 end
